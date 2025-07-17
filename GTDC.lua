f075d047110de908074f3abb7ef433a441bfda8e
jaycentral <lunarfixes@gmail.com>
2025-07-17 08:25:32 -0400

    Update GTDC.lua

--- GTDC.lua
+++ GTDC.lua
@@ -16,6 +16,55 @@ local Label = Tab:CreateButton({
 	Callback = function() end
 })
 
+local PlainsToggle = Tab:CreateToggle({
+	Name = "Plains Farm 🍅🍍",
+	CurrentValue = false,
+	Flag = "PlainsFarmToggle",
+	Callback = function(state)
+		if state then
+			getgenv().loopfarm = true
+			while getgenv().loopfarm do
+				local args = {"dif_easy"}
+				game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("PlaceDifficultyVote"):InvokeServer(unpack(args))
+
+				local args1 = {
+					"unit_tomato_plant",
+					{
+						Valid = true,
+						Rotation = 180,
+						CF = CFrame.new(-332.16180419921875, 62.845054626464844, -106.08424377441406, -1, 0, -8.742277657347586e-08, 0, 1, 0, 8.742277657347586e-08, 0, -1),
+						Position = Vector3.new(-332.16180419921875, 62.845054626464844, -106.08424377441406)
+					}
+				}
+				game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("PlaceUnit"):InvokeServer(unpack(args1))
+
+				local args2 = {
+					"unit_pineapple",
+					{
+						Valid = true,
+						Rotation = 180,
+						CF = CFrame.new(-332.2293701171875, 62.845054626464844, -113.29612731933594, -1, 0, -8.742277657347586e-08, 0, 1, 0, 8.742277657347586e-08, 0, -1),
+						Position = Vector3.new(-332.2293701171875, 62.845054626464844, -113.29612731933594)
+					}
+				}
+				game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("PlaceUnit"):InvokeServer(unpack(args2))
+
+				local args3 = {116}
+				game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args3))
+
+				local args4 = {111}
+				game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args4))
+
+				game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("RestartGame"):InvokeServer()
+
+				wait(5)
+			end
+		else
+			getgenv().loopfarm = false
+		end
+	end,
+})
+
 local TropicalToggle = Tab:CreateToggle({
 	Name = "Tropical Farm 🌾🌴",
 	CurrentValue = false,
